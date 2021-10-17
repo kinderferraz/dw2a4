3e630776e1d23e6d800333bed70b14976171ca8b
Alkindar Ferraz <alferraz@uolinc.com>
2021-10-17 15:06:56 -0300

    BugFix validação de data

--- a4/src/js/modules/validations.js
+++ a4/src/js/modules/validations.js
@@ -2,7 +2,7 @@ const rules = {
     email: email => email.match(/\S+@\S+\.\S+/) != null,
     date: date => {
         const splitDate = date.split("/")
-        if (splitDate.length < 2) return false
+        if (splitDate.length <= 2) return false
         return (splitDate[0] > 0 && splitDate[0] <= 31) &&
             (splitDate[1] > 0 && splitDate[1] <= 12) &&
             splitDate[2].length == 4
